3c6f5cd67a9c2da920b380d05f390e120acf31dc
Bsruiz01 <30706056+Bsruiz01@users.noreply.github.com>
2019-04-03 08:30:44 -0700

    Add files via upload

---
+++ length.java
@@ -0,0 +1,16 @@
+/*
+*Brian Ruiz
+*4-2-19
+*/
+import java.util.*;
+public class Length{
+ public static main(String[] args){   
+
+Scanner input = new Scanner(System.in);
+System.out.println("What is your name ");
+String name = input.next();
+System.out.println("Hello, " +name+ ".");
+int num = name.length();
+System.out.print("Your name has " +num+ "letters");
+}
+}
